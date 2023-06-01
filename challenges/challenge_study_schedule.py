39e659355d50b85606ceb3369ad3d48c867dc033
PirminP <pirmin.pernsteiner@gmail.com>
2023-06-01 15:46:36 -0300

    feat task1: return none if input permanence_period and target_time invalid or empty

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    #  studying_students = 0
+    for time_period in permanence_period:
+        if (
+            type(time_period[0]) != int
+            or type(time_period[1]) != int
+            or type(target_time) == 0
+        ):
+            return None
