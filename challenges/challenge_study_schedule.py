910b12f601ced9b5ee332920442bd4a6b63351a0
PirminP <pirmin.pernsteiner@gmail.com>
2023-06-01 16:12:30 -0300

    feat task1: return quantity of studying students within specific period of time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    #  studying_students = 0
+    studying_students = 0
     for time_period in permanence_period:
         if (
             type(time_period[0]) != int
             or type(time_period[1]) != int
-            or type(target_time) == 0
+            or type(target_time) != int
         ):
             return None
+
+        if time_period[0] <= target_time <= time_period[1]:
+            studying_students += 1
+
+    return studying_students
