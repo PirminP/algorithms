ebf523e5309c2744b73d1ac2396fa19c977c00a0
PirminP <pirmin.pernsteiner@gmail.com>
2023-06-02 15:34:54 -0300

    feat task2: Testing encrypt message file

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,22 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+
+    #  Testing valid input: even & uneven key
+    assert encrypt_message('Python', 3) == 'tyP_noh'
+    assert encrypt_message('Software', 6) == 'er_awtfoS'
+    assert encrypt_message('engineering', 5) == 'nigne_gniree'
+    assert encrypt_message('sweethome', 2) == 'emohtee_ws'
+
+    #  Testing invalid key type
+    with pytest.raises(TypeError):
+        encrypt_message('algorithm', 'key_invalid')
+
+    #  Testing invalid messsage type
+    with pytest.raises(TypeError):
+        encrypt_message(123456, 4)
+
+    #  Testing key outside definied range
+    assert encrypt_message('cheers', 8) == 'sreehc'
